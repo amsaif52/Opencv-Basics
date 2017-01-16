1d3440a0c057d9c1d7b034ff1954c557473348a4
amsaif52 <am52saif@gmail.com>
2017-01-15 22:35:04 -0500

    Added Graysacle and HSVscale in OpenCV basics

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cv = require('opencv');
 
+//Using cv#readImage() and Matrix#save()
 cv.readImage("./img/nasa.jpg",function(err,img){
 
   if(err){
@@ -13,6 +14,20 @@ cv.readImage("./img/nasa.jpg",function(err,img){
     throw new Error('Image has no size');
   }
 
+
+
+  //Using Matrix#Grayscale()
+
+  img.convertGrayscale();
+  img.gaussianBlur([3,3]);
   img.save("./img/myNewImage.jpg");
+
+  // img.save("./img/myNewImage1.jpg");
+  //
+  //
+  // // Using Matrix#HSVscale()
+  //
+  // img.convertHSVscale();
+  // img.save("./img/myNewImage2.jpg");
 
 });
